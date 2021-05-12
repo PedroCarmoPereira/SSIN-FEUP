fb321519c10a41130620d21ef4de649ece74742a
pedrocarmopereira <pppereira3@gmail.com>
2021-05-12 19:31:18 +0100

    Add table for stories about the country

--- Project/server.js
+++ Project/server.js
@@ -30,5 +30,20 @@ app.get("/api/users", (_, res, __) => {
     });
 });
 
+app.get("/api/stories", (_, res, __) => {
+  var sql = "select * from story"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
--- Project/src/db/populate.js
+++ Project/src/db/populate.js
@@ -30,35 +30,59 @@ function populate(){
 		CONSTRAINT email_unique UNIQUE (email)
 		)`,
 		(err) => {
-			if (err) {}
+			if (err) {
+				console.log(err);
+			}
 			else{
 				// Table just created, creating some rows
 				let insert = 'INSERT INTO user (name, email, username, password, access_lvl) VALUES (?,?,?, ?,?)';
-				db.run(insert, ["Admin Account","admin@example.com", "AdminAcc", bcrypt.hashSync("Admin1234567", SALT_LEN)], 3);
-				db.run(insert, ["Zé Ninguém","jose_ningue@example.com", "Zenin123", bcrypt.hashSync("Zenin1234567", SALT_LEN)], 0);
-				db.run(insert, ["José Centro","jose_intermed@example.com", "Zemid123", bcrypt.hashSync("Zemid1234567", SALT_LEN)], 1);
-				db.run(insert, ["Zezão Reizão","jose_reis@example.com", "Zegran123", bcrypt.hashSync("Zegran1234567", SALT_LEN)], 2);
-				db.run(insert, ["João Silva", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Joana Silva", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Pedro Silva", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Fábio Almeida", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Jeremias Dentro-da-Lei", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Generoso Primo", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Sérgio Portugal", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Alexandra Magnanima", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Mariana Ferreira", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Ada Cordão-Amor", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Carlos Babette", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Pedro Atleta", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 0);
-				db.run(insert, ["Fernando Tola", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 1);
-				db.run(insert, ["Cátia Vanessa", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 1);
-				db.run(insert, ["Vanessa Cátia", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 1);
-				db.run(insert, ["Silvestre Joanino", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 1);
-				db.run(insert, ["Dra. Beatriz Aranha", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN)], 2);
-				db.run(insert, ["Coronel Mostarda", generateEmail(), generateUname(), bcrypt.hashSync("SGketchup755", SALT_LEN)], 3);
-				db.run(insert, ["Tiago Ligação", generateEmail(), generateUname(), bcrypt.hashSync("AgitNMix4666", SALT_LEN)], 3);
+				let stmt = db.prepare(insert);
+				stmt.run(["Admin Account","admin@example.com", "AdminAcc", bcrypt.hashSync("Admin1234567", SALT_LEN), 3])
+				stmt.run(["Zé Ninguém","jose_ningue@example.com", "Zenin123", bcrypt.hashSync("Zenin1234567", SALT_LEN), 0]);
+				stmt.run(["José Centro","jose_intermed@example.com", "Zemid123", bcrypt.hashSync("Zemid1234567", SALT_LEN), 1]);
+				stmt.run(["Zezão Reizão","jose_reis@example.com", "Zegran123", bcrypt.hashSync("Zegran1234567", SALT_LEN), 2]);
+				stmt.run(["João Silva", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Joana Silva", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Pedro Silva", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Fábio Almeida", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Jeremias Dentro-da-Lei", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Generoso Primo", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Sérgio Portugal", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Alexandra Magnanima", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Mariana Ferreira", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Ada Cordão-Amor", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Carlos Babette", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Pedro Atleta", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 0]);
+				stmt.run(["Fernando Tola", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 1]);
+				stmt.run(["Cátia Vanessa", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 1]);
+				stmt.run(["Vanessa Cátia", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 1]);
+				stmt.run(["Silvestre Joanino", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 1]);
+				stmt.run(["Dra. Beatriz Aranha", generateEmail(), generateUname(), bcrypt.hashSync(generateId(), SALT_LEN), 2]);
+				stmt.run(["Coronel Mostarda", generateEmail(), generateUname(), bcrypt.hashSync("SGketchup755", SALT_LEN), 3]);
+				stmt.run(["Tiago Ligação", generateEmail(), generateUname(), bcrypt.hashSync("AgitNMix4666", SALT_LEN), 3]);
 			}
-	}); 
+	});
+	db.run(`CREATE TABLE story (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title text,
+		article text,
+		author_id INTEGER, 
+		FOREIGN KEY(author_id) REFERENCES user(id)
+		)`,
+		(err) => {
+			if (err) {
+				if (err.errno !== 1) throw err;
+			}
+			
+			let insert = 'INSERT INTO story (title, article, author_id) VALUES (?,?,?)';
+			let stmt = db.prepare(insert);
+			stmt.run('How it all began...', 'Some dude named Afonso decided Spain was shit...', 19);
+			stmt.run('How it all developed...', 'They killed some A-rabs, then Spain killed some Tugas, then D. Jonh killed some Spaniards, badabim badabum, Discoveries or as the rest of the world calls it the start of the Transatatlantic Slave Trade...', 19);
+			stmt.run('The fall...', 'The 1st Manuel decided it was a good idea to spend all our stolen money on a fucking marble church', 20);
+			stmt.run('The DickTator', 'Salazar Slitherin wanted to purge all half-bloods and wizards of muggle blood or some shit...', 18);
+			stmt.run('Where we at now', 'In debt but recovering, wait Corona-what?', 18);
+			stmt.run('Visit Portugal', 'We got beaches, we got bitches. We got all the Almeida\'s you could want. We also have a guy that videobombs reporters.', 20);
+	});
 }
 
 populate();
