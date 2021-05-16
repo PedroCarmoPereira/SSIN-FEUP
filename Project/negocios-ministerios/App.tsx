58c45f8d84c5111b2193ec97ae51fed5ee93ef9a
Christopher Fernandes de Abreu <cfa911@outlook.pt>
2021-05-16 18:00:17 +0100

    Added pngs and icons

--- Project/negocios-ministerios/App.tsx
+++ Project/negocios-ministerios/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>My dummy text!</Text>
+      <Image style={{width: 255 , height: 133, resizeMode: 'stretch',}} source={require("./assets/extended_icon.png")} /> 
       <StatusBar style="auto" />
     </View>
   );
