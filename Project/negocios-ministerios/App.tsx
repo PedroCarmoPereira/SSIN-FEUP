e5cb3922bdc9ca0fba8298123fe361a3ae114885
FilipeCarlosNogueira <43604899+FilipeCarlosNogueira@users.noreply.github.com>
2021-05-17 16:20:38 +0100

    react elements install

--- Project/negocios-ministerios/App.tsx
+++ Project/negocios-ministerios/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navigator from './app/routes/Navigation';
-import ChatSearch from './app/screens/ChatSearch'
 
 export default function App() {
-  return <ChatSearch />;
+  return <Navigator />;
 }
