3acfab7f1d0cb97b9e3e5e719057f84528776e10
pedrocarmopereira <pppereira3@gmail.com>
2021-05-18 19:08:44 +0100

    Protect users route

--- Project/api/routes/user.js
+++ Project/api/routes/user.js
@@ -1,18 +1,34 @@
 const db = require("../db/database.js");
 
 module.exports = (app) => {
-    app.get("/api/users", (_, res, __) => {
-        var sql = "select * from user"
-        var params = []
-        db.all(sql, params, (err, rows) => {
+    app.get("/api/users", (req, res, __) => {
+        if (req.header('Authorization') === undefined) {
+            res.status(401).json({ "error": "Unauthorized" });
+            return;
+        }
+        var find_user = "SELECT * FROM user WHERE token = ?";
+
+        db.get(find_user, [req.header('Authorization')], (err, row) => {
             if (err) {
                 res.status(400).json({ "error": err.message });
                 return;
             }
-            res.json({
-                "message": "success",
-                "data": rows
-            })
+            if (!row || row.access_lvl < 2) {
+                res.status(401).json({ "error": "Unauthorized" });
+                return;
+            }
+            var sql = "select * from user"
+            var params = []
+            db.all(sql, params, (err, rows) => {
+                if (err) {
+                    res.status(400).json({ "error": err.message });
+                    return;
+                }
+                res.json({
+                    "message": "success",
+                    "data": rows
+                })
+            });
         });
     });
 };
