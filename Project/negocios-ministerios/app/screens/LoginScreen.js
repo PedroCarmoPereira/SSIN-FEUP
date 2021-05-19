81cb5d6b66ec81aeddfd8c1ff75526d6adbbe154
FilipeCarlosNogueira <43604899+FilipeCarlosNogueira@users.noreply.github.com>
2021-05-19 03:54:49 +0100

    fetch attempt

--- Project/negocios-ministerios/app/screens/LoginScreen.js
+++ Project/negocios-ministerios/app/screens/LoginScreen.js
@@ -1,12 +1,24 @@
-import React, {useState} from 'react';
-import { Text, View, ImageBackground, Button } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import { Text, View, ImageBackground, Button, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import globalStyles from '../styles/globalStyles';
 import { api, storeToken } from '../utils/Api';
 
 function LoginScreen({navigation}) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+
+    const movieURL = "https://reactnative.dev/movies.json";
+
+    const [isLoading, setLoading] = useState([]);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(movieURL)
+            .then((response) => response.json())
+            .then((json) => setMovies(json.movies))
+            .catch((error) => alert(error))
+            .finally(setLoading(false));
+    });
+    
     const pressHandler = () => {
         api.post('/api/register', {
             username,
@@ -24,28 +36,41 @@ function LoginScreen({navigation}) {
     }
 
     return (
-        <ImageBackground source={require('../assets/Background.png')} style={globalStyles.container}>
+        <SafeAreaView flex={1}>
+            {isLoading ? (<ActivityIndicator/>) : (
+                <ImageBackground source={require('../assets/Background.png')} style={globalStyles.container}>
 
-            <Text>Login</Text>
+                    <Text>Login</Text>
+        
+                    <View>
+                        <Text>Email</Text>
+                        <TextInput style={globalStyles.TextInput}/>
+                        <Text>Password</Text>
+                        <View>
+                            <TextInput style={globalStyles.TextInput} secureTextEntry/>
+                        </View>
+                    </View>
+                    
+                    <Button
+                        title='Login'
+                        onPress={pressHandler}
+                        color="red"
+                        style={globalStyles.getStartedButton}
+                    />
 
-            <View>
-                <Text>Username</Text>
-                <TextInput onChangeText={setUsername}
-                value={username} style={globalStyles.TextInput}/>
-                <Text>Password</Text>
-                <View>
-                    <TextInput onChangeText={setPassword}
-                    value={password} style={globalStyles.TextInput} secureTextEntry/>
-                </View>
-            </View>
-            
-            <Button
-                title='Login'
-                onPress={pressHandler}
-                color="red"
-                style={globalStyles.getStartedButton}
-            />
-        </ImageBackground>
+                    <FlatList
+                        data={movies}
+                        keyExtractor={({id}, index) => id}
+                        renderItem={({item}) => (
+                            <Text>
+                                {item.title}
+                                {item.releaseYear}
+                            </Text>
+                        )}
+                    />
+                </ImageBackground>
+            )}
+        </SafeAreaView>
     );
 }
 
