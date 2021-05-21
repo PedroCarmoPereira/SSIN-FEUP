4428152030d88b4f19aa80d79ceeb0d367bf6614
Bernardo Santos <bernardo.santos10@gmail.com>
2021-05-22 00:12:19 +0100

    Add disable button when logging in

--- Project/negocios-ministerios/app/screens/LoginScreen.js
+++ Project/negocios-ministerios/app/screens/LoginScreen.js
@@ -1,25 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Text, View, ImageBackground, Button } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import globalStyles from '../styles/globalStyles';
 import { api, storeToken } from '../utils/Api';
 
-function LoginScreen({navigation}) {
+function LoginScreen({ navigation }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [disableSubmit, setDisableSubmit] = useState(false);
     const pressHandler = () => {
+        setDisableSubmit(true);
         api.post('/api/register', {
             username,
             password,
         })
-          .then(async (response) => {
-            if (response.status == 200) {
-                storeToken(response.data.token);
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+            .then(async (response) => {
+                console.log(response.data);
+                if (response.status == 200) {
+                    storeToken(response.data.token);
+                }
+            })
+            .catch(function (error) {
+                setDisableSubmit(false);
+                console.log(error);
+            });
 
     }
 
@@ -31,19 +35,20 @@ function LoginScreen({navigation}) {
             <View>
                 <Text>Username</Text>
                 <TextInput onChangeText={setUsername}
-                value={username} style={globalStyles.TextInput}/>
+                    value={username} style={globalStyles.TextInput} />
                 <Text>Password</Text>
                 <View>
                     <TextInput onChangeText={setPassword}
-                    value={password} style={globalStyles.TextInput} secureTextEntry/>
+                        value={password} style={globalStyles.TextInput} secureTextEntry />
                 </View>
             </View>
-            
+
             <Button
                 title='Login'
                 onPress={pressHandler}
                 color="red"
                 style={globalStyles.getStartedButton}
+                disabled={disableSubmit}
             />
         </ImageBackground>
     );
