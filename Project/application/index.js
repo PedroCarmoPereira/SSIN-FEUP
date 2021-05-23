99874460449281dec74867763799a66488c5c6d1
pedrocarmopereira <pppereira3@gmail.com>
2021-05-23 04:03:28 +0100

    Login done

--- Project/application/index.js
+++ Project/application/index.js
@@ -1,17 +1,27 @@
 const crypto = require('./encrypt');
+const login = require('./login');
+const fs = require('fs');
 
-const main = () => {
+const main = async () => {
 	//console.log(process.env.SERVER_URL);
-	require('./login');
+	login.login.then(() => {
+		console.log(login.getToken());
+	});
+	
 	/*
 	const plain = Buffer.from('Hello world');
 
 	const encrypted = crypto.encrypt(plain);
 	console.log('Encrypted:', encrypted.toString());
 
+	console.log("Enc: ", encrypted);
+
 	const decrypted = crypto.decrypt(encrypted);
 	console.log('Decrypted:', decrypted.toString());
-	*/
+	
+	fs.readFile('token', function(err, data) {
+
+	});*/
 }
 
 main();
--- Project/application/login.js
+++ Project/application/login.js
@@ -2,41 +2,51 @@ const fs = require('fs');
 const { api } = require('./api');
 const crypto = require('./encrypt');
 const prompt = require('prompt-sync')();
-   
 
-let token = "";
+let token;
 let flag = true;
+const login = new Promise ((resolve, reject) => {
+	fs.readFile('token', async function(err, data) {
+		if (err){
+			flag = false;
+			console.log("No account found on device.");
+			const username = prompt("Username: ");
+			const password = prompt.hide("Password: ");
+			await api.post('/api/register', {
+				username:username,
+				password:password,
+			})
+				.then(async (response) => {
+					if (response.status == 200) {
+						token = response.data.token;
+						resolve(token);
+					}
+				})
+				.catch(function (error) {
+					console.log("An error has occurred: " + error.response.data.error);
+					reject("A");
+				});
+		}
+		else {
+			token = crypto.decrypt(JSON.parse(data));
+			console.log("Logging you in...");
+			resolve(token);
+		}
 
-fs.readFile('token', function(err, data) {
-    if (err){
-		flag = false;
-		console.log("No account found on device.");
-		const username = prompt("Username: ");
-		const password = prompt.hide("Password: ");
-		console.log(username);
-		console.log(password);
-		api.post('/api/register', {
-            username:username,
-            password:password,
-        })
-            .then(async (response) => {
-                if (response.status == 200) {
-                    token = response.data.token;
-                }
-            })
-            .catch(function (error) {
-                console.log("An error has occurred: " + error.response.data.error);
-            });
-	}
-	else {
-		console.log("Logging you in...");
-		token = crypto.decrypt(data);
-	}
+		if (!flag){
+			fs.writeFile('token', JSON.stringify(crypto.encrypt(token)), (err) => {
+				if (err) console.log(err);
+				console.log("Saved account info.");
+			});
+		}
+	});
 });
 
-if (!flag){
-	fs.writeFile('token', crypto.encrypt(token), (err) => {
-		if (err) console.log(err);
-		console.log("Saved");
-	});
+const getToken = () => {
+	return token;
+}
+
+module.exports = {
+	login,
+	getToken,
 }
