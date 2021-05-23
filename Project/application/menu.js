fe7a141e5aa5bf11dca799c11ac20264b8018500
pedrocarmopereira <pppereira3@gmail.com>
2021-05-23 13:36:11 +0100

    Add getStories

--- Project/application/menu.js
+++ Project/application/menu.js
@@ -5,17 +5,17 @@ const mainMenu = (access_lvl) => {
 	console.log("0. Exit App");
 	console.log("---------------------------");
 	console.log("1. View Stories");
-	if(access_lvl >= 1){
+	if (access_lvl >= 1) {
 		console.log("2. Publish Story");
 		console.log("3. Delete Story");
 	}
 	console.log("---------------------------");
-	if(access_lvl >= 1)
+	if (access_lvl >= 1)
 		console.log("4. View Appointments");
 	console.log("5. Set Appointment");
-	if(access_lvl >= 1)
+	if (access_lvl >= 1)
 		console.log("6. Delete Appointment");
-	if(access_lvl == 3){
+	if (access_lvl == 3) {
 		console.log("---------------------------");
 		console.log("7. View deliveries");
 		console.log("8. View delivery");
@@ -24,12 +24,12 @@ const mainMenu = (access_lvl) => {
 		console.log("11. Delete delivery");
 	}
 	console.log("---------------------------");
-	if(access_lvl >= 2){
+	if (access_lvl >= 2) {
 		console.log("12. View visas");
 		console.log("13. View visa");
 	}
 	console.log("14. Request visa");
-	if(access_lvl >= 2){
+	if (access_lvl >= 2) {
 		console.log("15. Approve visa");
 		console.log("16. Delete visa");
 	}
